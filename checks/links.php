be6b1a34609da77f278bbba351a2da42e15fd9f7
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-11 16:36:57 +0000

    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@310469 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/links.php
+++ checks/links.php
@@ -13,8 +13,12 @@ class Check_Links implements themecheck {
 		foreach ($php_files as $php_key => $phpfile) {
 		checkcount();
 		$grep = '';
-			if ( preg_match_all( '/(<a\s[^>]*?title=([\'"])?([^>]*?(?=\2))\2?([^>]*?)>)(.*?)(<\/a>)/is', $phpfile, $out, PREG_SET_ORDER ) ) {
-			    $filename = basename($php_key);
+		// regex borrowed from TAC
+		$url_re='([[:alnum:]\-\.])+(\\.)([[:alnum:]]){2,4}([[:blank:][:alnum:]\/\+\=\%\&\_\\\.\~\?\-]*)';
+		$title_re='[[:blank:][:alnum:][:punct:]]*';	// 0 or more: any num, letter(upper/lower) or any punc symbol
+		$space_re='(\\s*)'; 
+if ( preg_match_all( "/(<a)(\\s+)(href".$space_re."=".$space_re."\"".$space_re."((http|https|ftp):\\/\\/)?)".$url_re."(\"".$space_re.$title_re.$space_re.">)".$title_re."(<\\/a>)/is", $phpfile, $out, PREG_SET_ORDER ) ) {			
+			$filename = basename($php_key);
 			    foreach( $out as $key ) {
 				if ( !strpos( $key[0], 'http://wordpress.org/' ) ) {
 				$grep .= tc_grep( $key[0], $php_key);	
