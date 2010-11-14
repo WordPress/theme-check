ee525158d4b90c5789b63d5d528da01a06a9d99e
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 14:40:13 +0000

    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@311307 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/style_needed.php
+++ checks/style_needed.php
@@ -3,14 +3,10 @@
 // search for some bad things
 class Style_Needed implements themecheck {
 	protected $error = array();
-
 	function check( $php_files, $css_files, $other_files) {
-
 		// combine all the php files into one string to make it easier to search
 		$css = implode(' ', $css_files);
-
 		$ret = true;
-
 		// things to check for
 		$checks = array(
 		'^Theme Name:' => '<strong>Theme name:</strong> is missing from your style.css header.',
@@ -26,24 +22,18 @@ class Style_Needed implements themecheck {
 		'\.sticky' => '<strong>.sticky</strong> css class is needed in your theme css.',
 		'\.bypostauthor' => '<strong>.bypostauthor</strong> css class is needed in your theme css.',
 		'\.alignright' => '<strong>.alignright</strong> css class is needed in your theme css.'
+		'\.sticky' => '.sticky',
+		'\.bypostauthor' => '.bypostauthor'
 			);
-
-
 		foreach ($checks as $key => $check) {
 		checkcount();
 			if ( !preg_match( '/' . $key . '/mi', $css, $matches ) ) {
 				$this->error[] = "CSSNEEDED{$check}";
 				$ret = false;
 			}
-
-
 		}
-
-
 		return $ret;
 	}
-
 	function getError() { return $this->error; }
 }
-
 $themechecks[] = new Style_Needed;
