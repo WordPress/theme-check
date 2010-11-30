c351ce4be6347bdc0a728137de412dcb2419926b
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-30 20:21:54 +0000

    Fix post format check. Should have been &&'s, and also needs to check for use of .format-* in CSS files (for themes that use formats entirely via styling).
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@317127 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/post-formats.php
+++ checks/post-formats.php
@@ -6,8 +6,9 @@ class PostFormatCheck implements themecheck {
 	function check( $php_files, $css_files, $other_files) {
 		$ret = true;
 		
-		// combine all the php files into one string to make it easier to search
 		$php = implode(' ', $php_files);
+		$css = implode(' ', $css_files);
+
 		checkcount();
 
 		$checks = array(
@@ -18,11 +19,11 @@ class PostFormatCheck implements themecheck {
 			foreach ($checks as $check) {
 				checkcount();
 				if ( preg_match( $check, $phpfile, $matches ) ) {
-					if ( !strpos( $php, 'get_post_format' ) || !strpos( $php, 'has_post_format' ) ) {
+					if ( !strpos( $php, 'get_post_format' ) && !strpos( $php, 'has_post_format' ) && !strpos( $css, '.format' ) ) {
 						$filename = basename($php_key);
 						$error = esc_html( rtrim($matches[0],'(') );
 						$grep = tc_grep( rtrim($matches[0],'('), $php_key);
-						$this->error[] = "REQUIRED<strong>{$error}</strong> was found in the file <strong>{$filename}</strong>. However get_post_format and/or has_post_format were not found!";
+						$this->error[] = "REQUIRED<strong>{$error}</strong> was found in the file <strong>{$filename}</strong>. However get_post_format and/or has_post_format were not found, and no use of formats in the CSS was detected.";
 						$ret = false;
 					}
 				}
