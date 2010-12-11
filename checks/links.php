5670438d5f89c1ab57f913d867ecc84f579c9af7
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-11 23:33:07 +0000

    duplicates can be repetative!
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@321799 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/links.php
+++ checks/links.php
@@ -8,22 +8,21 @@ class Check_Links implements themecheck {
 
 		foreach ($php_files as $php_key => $phpfile) {
 			checkcount();
-			
+
 			$grep = '';
-			
+
 			// regex borrowed from TAC
 			$url_re='([[:alnum:]\-\.])+(\\.)([[:alnum:]]){2,4}([[:blank:][:alnum:]\/\+\=\%\&\_\\\.\~\?\-]*)';
 			$title_re='[[:blank:][:alnum:][:punct:]]*';	// 0 or more: any num, letter(upper/lower) or any punc symbol
-			$space_re='(\\s*)'; 
-			if ( preg_match_all( "/(<a)(\\s+)(href".$space_re."=".$space_re."\"".$space_re."((http|https|ftp):\\/\\/)?)".$url_re."(\"".$space_re.$title_re.$space_re.">)".$title_re."(<\\/a>)/is", $phpfile, $out, PREG_SET_ORDER ) ) {			
+			$space_re='(\\s*)';
+			if ( preg_match_all( "/(<a)(\\s+)(href".$space_re."=".$space_re."\"".$space_re."((http|https|ftp):\\/\\/)?)".$url_re."(\"".$space_re.$title_re.$space_re.">)".$title_re."(<\\/a>)/is", $phpfile, $out, PREG_SET_ORDER ) ) {
 				$filename = basename($php_key);
 				foreach( $out as $key ) {
 					if ( $key[0] && !strpos( $key[0], 'http://wordpress.org' ) ) {
-						$grep .= tc_grep( $key[0], $php_key);	
+						$grep .= tc_grep( $key[0], $php_key);
+							}
+						}
 						$this->error[] = "<span class='tc-lead tc-info'>INFO</span>: Possible hard-coded links were found in the file <strong>{$filename}</strong>.{$grep}";
-						$ret = false;				
-					}
-				}
 			}
 		}
 		return $ret;
