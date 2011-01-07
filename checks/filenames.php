4fd3cb06d8635c4211e34c5f8732f1dda357303d
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-07 19:30:12 +0000

    readme.txt explanations
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@329873 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/filenames.php
+++ checks/filenames.php
@@ -19,7 +19,7 @@ class File_Checks implements themecheck {
 		}
 
 		$musthave = array( 'index.php', 'comments.php', 'screenshot.png', 'style.css' );
-		$rechave = array( 'readme.txt' );
+		$rechave = array( 'readme.txt' => ' Please see <a href = "http://codex.wordpress.org/Theme_Review#Theme_Documentation">Theme_Documentation</a> for more information.' );
 
 		checkcount();		
 
@@ -30,9 +30,9 @@ class File_Checks implements themecheck {
 			}
 		}
 
-		foreach( $rechave as $file ) {
+		foreach( $rechave as $file => $reason ) {
 			if ( !in_array( $file, $filenames ) ) {
-				$this->error[] = "<span class='tc-lead tc-recommended'>RECOMMENDED</span>: could not find the file <strong>{$file}</strong> in the theme.";
+				$this->error[] = "<span class='tc-lead tc-recommended'>RECOMMENDED</span>: could not find the file <strong>{$file}</strong> in the theme.{$reason}";
 				$ret = false;
 			}
 		}		
