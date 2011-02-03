4aa53f14011f7ea2f70372c91168e6b47a51b27f
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-03 19:12:21 +0000

    fix textdomain checker, now detects no domain.
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@340409 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/textdomain.php
+++ checks/textdomain.php
@@ -11,22 +11,20 @@ class TextDomainCheck implements themecheck {
 
 		if ( strpos( $css, 'Theme Name: Twenty Ten' ) ) return $ret;
 
-		$checks1 = '/(_[_|e]\([^,;\r\n]*,[^\)]*\))/';
-		foreach ( $php_files as $php_key => $phpfile ) {
-			checkcount();
-			if ( preg_match_all( $checks1, $phpfile, $matches ) ) {
+		$checks = array(
+		'/_[e|_]\([^,]*,\s?[\'|"]twentyten[\'|"]\s?\)/' => 'twentyten text domain is being used!', 
+		'/_[e|_]\(\s?[\'|"][^\'|"]*[\'|"]\s?\);/' => 'You have not included a text domain!' );
 
-				if ( $out = preg_grep( '/twentyten/', $matches[0] ) ) {
-					$filename = tc_filename( $php_key );
-					foreach( $out as $error ) {
-						$grep .= tc_grep( $error , $php_key );
-// html_print_r($out);
-					}
-				$this->error[] = "<span class='tc-lead tc-warning'>WARNING</span>: Theme is using <strong>twentyten</strong> as the textdomain in <strong>{$filename}</strong>.{$grep}";
+		foreach ( $php_files as $php_key => $phpfile ) {
+		foreach ( $checks as $key => $check ) {
+		checkcount();
+			if ( preg_match( $key, $phpfile, $matches ) ) {
+				$filename = tc_filename( $php_key );
+				$error = tc_grep( $matches[0], $php_key );
+				$this->error[] = "<span class='tc-lead tc-warning'>WARNING</span>: Text domain problems in <strong>{$filename}</strong>. {$check}{$error}";
 				$ret = false;
 				}
 			}
-
 		}
 		return $ret;
 	}
