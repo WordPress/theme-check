0dc916e5d1990f67d920499b860ac61c3c30aa49
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-16 18:03:48 +0000

    rm one of the checks that causes problems on wporg uploader (and which is also unnecessary)
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@360847 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/time_date.php
+++ checks/time_date.php
@@ -7,19 +7,18 @@ class Time_Date implements themecheck {
 		$ret = true;
 
 		$checks = array(
-		'/\sdate_i18n\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'date_i18n( get_option( \'date_format\' ) )',
-		'/\sdate\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'date( get_option( \'date_format\' ) )',
-		'/[^get_]the_date\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'the_date( get_option( \'date_format\' ) )',
-		'/[^get_]the_time\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'the_time( get_option( \'date_format\' ) )'
+			'/\sdate_i18n\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'date_i18n( get_option( \'date_format\' ) )',
+			'/[^get_]the_date\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'the_date( get_option( \'date_format\' ) )',
+			'/[^get_]the_time\(\s?["|\'][A-Za-z\s]+\s?["|\']\)/' => 'the_time( get_option( \'date_format\' ) )'
 			);
 
 		foreach ( $php_files as $php_key => $phpfile ) {
-		foreach ( $checks as $key => $check ) {
-		checkcount();
-			if ( preg_match( $key, $phpfile, $matches ) ) {
-				$filename = tc_filename( $php_key );
-				$error = trim( esc_html( rtrim( $matches[0], '(' ) ) );
-				$this->error[] = '<span class="tc-lead tc-info">INFO</span>: ' . sprintf( __( 'At least one hard coded date was found in the file <strong>%s</strong>. Consider get_option( \'date_format\' )', 'themecheck' ), $filename );
+			foreach ( $checks as $key => $check ) {
+				checkcount();
+				if ( preg_match( $key, $phpfile, $matches ) ) {
+					$filename = tc_filename( $php_key );
+					$error = trim( esc_html( rtrim( $matches[0], '(' ) ) );
+					$this->error[] = '<span class="tc-lead tc-info">INFO</span>: ' . sprintf( __( 'At least one hard coded date was found in the file <strong>%s</strong>. Consider get_option( \'date_format\' )', 'themecheck' ), $filename );
 				}
 			}
 		}
