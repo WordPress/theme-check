96575c525f74b351624365bdeafe633bd05ab003
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-05 15:22:17 +0000

    Change version numbers
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@419789 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme-check.php
+++ theme-check.php
@@ -5,7 +5,7 @@ Plugin URI: http://pross.org.uk/plugins
 Description: A simple and easy way to test your theme for all the latest WordPress standards and practices. A great theme development tool!
 Author: Pross
 Author URI: http://pross.org.uk
-Version: 20110602.2
+Version: 20110805.1
 */
 
 add_action( 'admin_init', 'tc_i18n' );
