cf8eecbd05fba84a6a33e4cfacbf9453ecaaf713
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-21 17:33:29 +0000

    Checking for TEMPLATEPATH and STYLESHEETPATH props Chip.
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@426765 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ checks/constants.php
@@ -0,0 +1,34 @@
+<?php
+
+class Constants implements themecheck {
+	protected $error = array();
+
+	function check( $php_files, $css_files, $other_files ) {
+
+		$ret = true;
+
+		$checks = array(
+			'STYLESHEETPATH' => 'get_stylesheet_directory()',
+			'TEMPLATEPATH' => 'get_template_directory()',
+			'PLUGINDIR' => 'WP_PLUGIN_DIR',
+			'MUPLUGINDIR' => 'WPMU_PLUGIN_DIR'
+			);
+
+		foreach ( $php_files as $php_key => $phpfile ) {
+			foreach ( $checks as $key => $check ) {
+				checkcount();
+				if ( preg_match( '/[\s|]' . $key . '/', $phpfile, $matches ) ) {
+					$filename = tc_filename( $php_key );
+					$error = ltrim( rtrim( $matches[0], '(' ) );
+					$grep = tc_grep( $error, $php_key );
+					$this->error[] = "<span class='tc-lead tc-info'>INFO</span>: <strong>{$error}</strong> was found in the file <strong>{$filename}</strong>. Use <strong>{$check}</strong> instead.{$grep}";
+					$ret = false;
+				}
+			}
+		}
+		return $ret;
+	}
+
+	function getError() { return $this->error; }
+}
+$themechecks[] = new Constants;
