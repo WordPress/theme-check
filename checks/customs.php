ae6191e33243a901e961a3f58405cc54f380bd5e
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-18 18:31:23 +0000

    3.4 support added
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@533026 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/customs.php
+++ checks/customs.php
@@ -10,12 +10,12 @@ class CustomCheck implements themecheck {
 
 		checkcount();
 
-		if ( strpos( $php, 'add_custom_image_header' ) === false ) {
-			$this->error[] = __( "<span class='tc-lead tc-recommended'>RECOMMENDED</span>: No reference to <strong>add_custom_image_header</strong> was found in the theme. It is recommended that the theme implement this functionality if using an image for the header.", "themecheck" );
+		if ( ! preg_match( '#add_theme_support\s?\(\s?[\'|"]custom-header#', $php ) ) {
+			$this->error[] = __( "<span class='tc-lead tc-recommended'>RECOMMENDED</span>: No reference to <strong>add_theme_support( 'custom-header', \$args )</strong> was found in the theme. It is recommended that the theme implement this functionality if using an image for the header.", "themecheck" );
 		}
 
-		if ( strpos( $php, 'add_custom_background' ) === false ) {
-			$this->error[] = __( "<span class='tc-lead tc-recommended'>RECOMMENDED</span>: No reference to <strong>add_custom_background()</strong> was found in the theme. If the theme uses background images or solid colors for the background, then it is recommended that the theme implement this functionality.", "themecheck" );
+		if ( ! preg_match( '#add_theme_support\s?\(\s?[\'|"]custom-background#', $php ) ) {
+			$this->error[] = __( "<span class='tc-lead tc-recommended'>RECOMMENDED</span>: No reference to <strong>add_theme_support( 'custom-background', \$args )</strong> was found in the theme. If the theme uses background images or solid colors for the background, then it is recommended that the theme implement this functionality.", "themecheck" );
 		}
 
 		return $ret;
--- checks/dep_recommend.php
+++ checks/dep_recommend.php
@@ -9,7 +9,23 @@ class Deprecated_Recommended implements themecheck {
 		$ret = true;
 
 		$checks = array(
-			array( 'wp_timezone_supported' => 'No alternatives', '3.2' )
+			// frontend
+			array( 'get_themes' => 'wp_get_themes()', '3.4' ),
+			array( 'get_theme' => 'wp_get_theme()', '3.4' ),
+			array( 'get_current_theme' => 'wp_get_theme()', '3.4' ),
+			array( 'clean_pre' => 'none available', '3.4' ),
+			array( 'add_custom_image_header' => 'add_theme_support( \'custom-header\', $args )', '3.4' ),
+			array( 'remove_custom_image_header' => 'remove_theme_support( \'custom-header\' )', '3.4' ),
+			array( 'add_custom_background' => 'add_theme_support( \'custom-background\', $args )', '3.4' ),
+			array( 'remove_custom_background' => 'remove_theme_support( \'custom-background\' )', '3.4' ),
+			array( 'get_theme_data' => 'wp_get_theme()', '3.4' ),
+			array( 'update_page_cache' => 'update_post_cache()', '3.4' ),
+			array( 'clean_page_cache' => 'update_post_cache()', '3.4' ),
+			
+			// admin
+			array( 'get_allowed_themes' => 'wp_get_themes( array( \'allowed\' => true ) )', '3.4' ),
+			array( 'get_broken_themes' => 'wp_get_themes( array( \'errors\' => true )', '3.4' ),
+			array( 'current_theme_info' => 'wp_get_theme()', '3.4' ),
 			);
 
 		foreach ( $php_files as $php_key => $phpfile ) {
