7d1651a90bcc0479d135cad23deb025e465b8ed9
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-13 21:42:58 +0000

    eliminate potential warning from token_get_all if the string isn't valid php for whatever reason
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@557586 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/i18n.php
+++ checks/i18n.php
@@ -17,19 +17,20 @@ class I18NCheck implements themecheck {
 			$error='';
 			if ( preg_match_all( '/_[e|_]\s?\([^\)]*\){1}?/', $phpfile, $matches ) ) {
 				foreach ( $matches[0] as $match ) {					
-					$tokens = token_get_all('<?php '.$match.';');
-					foreach ($tokens as $token) {
-						if (is_array($token) && in_array( $token[0], array( T_VARIABLE ) ) ) {
-							$filename = tc_filename( $php_key );
-							$grep = tc_grep( ltrim( $match ), $php_key );
-							preg_match( '/[^\s]*\s[0-9]+/', $grep, $line);
-							$error = ( !strpos( $error, $line[0] ) ) ? $grep : '';	
-							$this->error[] = sprintf(__('<span class="tc-lead tc-recommended">RECOMMENDED</span>: Possible variable <strong>%1$s</strong> found in translation function in <strong>%2$s</strong>. Translation function calls must NOT contain PHP variables. %3$s','themecheck'),
-								$token[1], $filename, $error);
-							break; // stop looking at the tokens on this line once a variable is found
+					$tokens = @token_get_all('<?php '.$match.';');
+					if (!empty($tokens)) {
+						foreach ($tokens as $token) {
+							if (is_array($token) && in_array( $token[0], array( T_VARIABLE ) ) ) {
+								$filename = tc_filename( $php_key );
+								$grep = tc_grep( ltrim( $match ), $php_key );
+								preg_match( '/[^\s]*\s[0-9]+/', $grep, $line);
+								$error = ( !strpos( $error, $line[0] ) ) ? $grep : '';	
+								$this->error[] = sprintf(__('<span class="tc-lead tc-recommended">RECOMMENDED</span>: Possible variable <strong>%1$s</strong> found in translation function in <strong>%2$s</strong>. Translation function calls must NOT contain PHP variables. %3$s','themecheck'),
+									$token[1], $filename, $error);
+								break; // stop looking at the tokens on this line once a variable is found
+							}
 						}
-					}
-					
+					}					
 				}
 			}
 				
