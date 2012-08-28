137a8ee61d747a95c914d7cad27657845ceab46a
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-28 15:06:44 +0000

    add flexible-header tag
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@591387 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/style_tags.php
+++ checks/style_tags.php
@@ -17,11 +17,7 @@ class Style_Tags implements themecheck {
 			return $ret;
 		}
 
-		$allowed_tags = array("black","blue","brown","gray","green","orange","pink","purple","red","silver","tan","white","yellow","dark","light","one-column",
-		"two-columns","three-columns","four-columns","left-sidebar","right-sidebar","fixed-width","flexible-width","blavatar","buddypress","custom-background",
-		"custom-colors","custom-header","custom-menu","editor-style","featured-image-header","featured-images","front-page-post-form","full-width-template",
-		"microformats","post-formats","rtl-language-support","sticky-post","theme-options","threaded-comments","translation-ready","holiday","photoblogging",
-		"seasonal");
+		$allowed_tags = array("black","blue","brown","gray","green","orange","pink","purple","red","silver","tan","white","yellow","dark","light","one-column","two-columns","three-columns","four-columns","left-sidebar","right-sidebar","fixed-width","flexible-width","flexible-header", "blavatar","buddypress","custom-background","custom-colors","custom-header","custom-menu","editor-style","featured-image-header","featured-images","front-page-post-form","full-width-template","microformats","post-formats","rtl-language-support","sticky-post","theme-options","threaded-comments","translation-ready","holiday","photoblogging","seasonal");
 		
 		foreach( $data[ 'Tags' ] as $tag ) {
 			if ( !in_array( strtolower( $tag ), $allowed_tags ) ) {
