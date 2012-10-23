3d2bffa59a881332b7773b5e9be371bac9057f84
Otto42 <Otto42@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 22:51:23 +0000

    bigger screenshots are okay
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@616581 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/screenshot.php
+++ checks/screenshot.php
@@ -17,8 +17,14 @@ class Screenshot_Checks implements themecheck {
 				if ( basename( $other_key ) === 'screenshot.png' && preg_match( '/.*themes\/[^\/]*\/screenshot\.png/', $other_key ))  {
 					// we have or screenshot!
 					$image = getimagesize( $other_key );
-					if ( $image[0] > 320 || $image[1] > 240 ) {
-						$this->error[] = sprintf(__('<span class="tc-lead tc-recommended">RECOMMENDED</span>: Screenshot is wrong size! Detected: <strong>%1$sx%2$spx</strong>. Maximum allowed size is 320x240px.', 'themecheck'), $image[0], $image[1]);
+					if ( $image[0] > 640 || $image[1] > 480 ) {
+						$this->error[] = sprintf(__('<span class="tc-lead tc-recommended">RECOMMENDED</span>: Screenshot is wrong size! Detected: <strong>%1$sx%2$spx</strong>. Maximum allowed size is 640x480px.', 'themecheck'), $image[0], $image[1]);
+					}
+					if ( $image[1] / $image[0] != 0.75 ) {
+						$this->error[] = __('<span class="tc-lead tc-recommended">RECOMMENDED</span>: Screenshot dimensions are wrong! Ratio of width to height should be 4:3.', 'themecheck');
+					}
+					if ( $image[0] != 600 || $image[1] != 450 ) {
+						$this->error[] = __('<span class="tc-lead tc-recommended">RECOMMENDED</span>: Screenshot size should be 600x450, to account for HiDPI displays. Any 4:3 image size is acceptable, but 600x450 is preferred.', 'themecheck');
 					}
 				}
 			}
