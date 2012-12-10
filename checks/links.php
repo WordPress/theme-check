2a269d7ba42189dcf0d57b818ea342f1e905bcfc
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-10 23:16:17 +0000

    Fix a warning in php 5.4
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@636824 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- checks/links.php
+++ checks/links.php
@@ -15,7 +15,6 @@ class Check_Links implements themecheck {
 			$space_re = '(\\s*)';
 			if ( preg_match_all( "/(<a)(\\s+)(href" . $space_re . "=" . $space_re . "\"" . $space_re . "((http|https|ftp):\\/\\/)?)" . $url_re . "(\"" . $space_re . $title_re . $space_re . ">)" . $title_re . "(<\\/a>)/is", $phpfile, $out, PREG_SET_ORDER ) ) {
 				$filename = tc_filename( $php_key );
-				$out = array_unique( $out );
 				foreach( $out as $key ) {
 					if ( preg_match( '/\<a\s?href\s?=\s?["|\'](.*?)[\'|"](.*?)\>(.*?)\<\/a\>/is', $key[0], $stripped ) ) {
 						if ( !empty( $data['AuthorURI'] ) && !empty( $data['URI'] ) && $stripped[1] && !strpos( $stripped[1], $data['URI'] ) && !strpos( $stripped[1], $data['AuthorURI'] ) && !strpos( $stripped[1], 'wordpress.' ) ) {
