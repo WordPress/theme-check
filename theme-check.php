301a4e995f8741c48cda2a4ae56c6d3f3114ab80
pross <pross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-23 16:36:03 +0000

    Move css to assets folder, fix missing image in css. Create a class ThemeCheckMain, fixes duplicate function load_styles reported in forums, plus looks better.
    git-svn-id: http://plugins.svn.wordpress.org/theme-check/trunk@920047 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme-check.php
+++ theme-check.php
@@ -9,50 +9,55 @@ Version: 20131213.1
 Text Domain: theme-check
 */
 
-add_action( 'admin_init', 'tc_i18n' );
-
-function tc_i18n() {
-	load_plugin_textdomain( 'theme-check', false, 'theme-check/lang' );
-}
-
-function load_styles() {
-	wp_enqueue_style('style', plugins_url( 'style.css', __FILE__ ), null, null, 'screen');
-}
-
-add_action( 'admin_menu', 'themecheck_add_page' );
-function themecheck_add_page() {
-	$page = add_theme_page( 'Theme Check', 'Theme Check', 'manage_options', 'themecheck', 'themecheck_do_page' );
-	add_action('admin_print_styles-' . $page, 'load_styles');
-}
-
-function tc_add_headers( $extra_headers ) {
-	$extra_headers = array( 'License', 'License URI', 'Template Version' );
-	return $extra_headers;
-}
-
-function themecheck_do_page() {
-	if ( !current_user_can( 'manage_options' ) )  {
-	wp_die( __( 'You do not have sufficient permissions to access this page.', 'theme-check' ) );
+class ThemeCheckMain {
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'tc_i18n' ) );
+		add_action( 'admin_menu', array( $this, 'themecheck_add_page' ) );
 	}
 
-	add_filter( 'extra_theme_headers', 'tc_add_headers' );
-
-	include 'checkbase.php';
-	include 'main.php';
-
-	echo '<div id="theme-check" class="wrap">';
-	echo '<div id="icon-themes" class="icon32"><br /></div><h2>Theme-Check</h2>';
-	echo '<div class="theme-check">';
-		tc_form();
-	if ( !isset( $_POST[ 'themename' ] ) )  {
-		tc_intro();
-
+	function tc_i18n() {
+		load_plugin_textdomain( 'theme-check', false, 'theme-check/lang' );
 	}
 
-	if ( isset( $_POST[ 'themename' ] ) ) {
-		if ( isset( $_POST[ 'trac' ] ) ) define( 'TC_TRAC', true );
-		check_main( $_POST[ 'themename' ] );
+	function load_styles() {
+		wp_enqueue_style('style', plugins_url( 'assets/style.css', __FILE__ ), null, null, 'screen');
 	}
-	echo '</div> <!-- .theme-check-->';
-	echo '</div>';
+
+	function themecheck_add_page() {
+		$page = add_theme_page( 'Theme Check', 'Theme Check', 'manage_options', 'themecheck', array( $this, 'themecheck_do_page' ) );
+		add_action('admin_print_styles-' . $page, array( $this, 'load_styles' ) );
+	}
+
+	function tc_add_headers( $extra_headers ) {
+		$extra_headers = array( 'License', 'License URI', 'Template Version' );
+		return $extra_headers;
+	}
+
+	function themecheck_do_page() {
+		if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.', 'theme-check' ) );
+		}
+
+		add_filter( 'extra_theme_headers', array( $this, 'tc_add_headers' ) );
+
+		include 'checkbase.php';
+		include 'main.php';
+
+		echo '<div id="theme-check" class="wrap">';
+		echo '<div id="icon-themes" class="icon32"><br /></div><h2>Theme-Check</h2>';
+		echo '<div class="theme-check">';
+			tc_form();
+		if ( !isset( $_POST[ 'themename' ] ) )  {
+			tc_intro();
+
+		}
+
+		if ( isset( $_POST[ 'themename' ] ) ) {
+			if ( isset( $_POST[ 'trac' ] ) ) define( 'TC_TRAC', true );
+			check_main( $_POST[ 'themename' ] );
+		}
+		echo '</div> <!-- .theme-check-->';
+		echo '</div>';
+	}
 }
+new ThemeCheckMain;
