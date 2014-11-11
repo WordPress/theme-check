fdcdc11f1eaf5bb16e1c766da60ad25e492d70a5
ryelle <kelly.dwan@gmail.com>
2014-11-11 16:10:59 -0500

    Check for a textdomain in __(), _e(), _x(), _n(), and _nx().

--- checks/textdomain.php
+++ checks/textdomain.php
@@ -11,26 +11,22 @@ class TextDomainCheck implements themecheck {
 		if ( $data['Name'] === 'Twenty Ten' || $data['Name'] === 'Twenty Eleven')
 			return $ret;
 
-		$checks = array(
-		'/[\s|\(|;]_[e|_]\s?\(\s?[\'|"][^\'|"]*[\'|"]\s?\)/' => __( 'You have not included a text domain!', 'theme-check' )
-		 );
+		// Checks for a textdomain in __(), _e(), _x(), _n(), and _nx().
+		$textdomain_regex = '/[\s\(]_x\s?\(\s?[\'"][^\'"]*[\'"]\s?,\s?[\'"][^\'"]*[\'"]\s?\)|[\s\(;]_[_e]\s?\(\s?[\'"][^\'"]*[\'"]\s?\)|[\s\(]_n\s?\(\s?[\'"][^\'"]*[\'"]\s?,\s?[\'"][^\'"]*[\'"]\s?,\s?[$a-z\(\)0-9]*\s?\)|[\s\(]_nx\s?\(\s?[\'"][^\'"]*[\'"]\s?,\s?[\'"][^\'"]*[\'"]\s?,\s?[$a-z\(\)0-9]*\s?,\s?[\'"][^\'"]*[\'"]\s?\)/';
 
-		foreach ( $php_files as $php_key => $phpfile ) {
+		foreach ( $php_files as $file_path => $file_contents ) {
 			$error = '';
-			foreach ( $checks as $key => $check ) {
-				checkcount();
-				if ( preg_match_all( $key, $phpfile, $matches ) || preg_match_all( '/[\s|\(]_x\s?\(\s?[\'|"][^\'|"]*[\'|"]\s?,\s?[\'|"][^\'|"]*[\'|"]\s?\)/', $phpfile, $matches )) {
 
-					$filename = tc_filename( $php_key );
-					foreach ($matches[0] as $match ) {
-						$grep = tc_grep( ltrim( $match ), $php_key );
-						preg_match( '/[^\s]*\s[0-9]+/', $grep, $line);
-						$error .= ( !strpos( $error, $line[0] ) ) ? $grep : '';
-					}
-				$this->error[] = sprintf( "<span class='tc-lead tc-recommended'>" . __( 'RECOMMENDED', 'theme-check' ) . '</span>: ' .
-					/* translators: 1: filename 2: error message 3: grep results */
-					__( 'Text domain problems in <strong>%1$s</strong>. %2$s %3$s ', 'theme-check' ), $filename, $check, $error );
+			if ( preg_match_all( $textdomain_regex, $file_contents, $matches ) ) {
+				$filename = tc_filename( $file_path );
+				foreach ( $matches[0] as $match ) {
+					$grep = tc_grep( ltrim( $match ), $file_path );
+					preg_match( '/[^\s]*\s[0-9]+/', $grep, $line );
+					$error .= ( ! strpos( $error, $line[0] ) ) ? $grep: '';
 				}
+
+				/* translators: 1: filename 2: grep results */
+				$this->error[] = sprintf( "<span class='tc-lead tc-recommended'>" . __( 'RECOMMENDED', 'theme-check' ) . '</span>: ' . __( 'Text domain problems in <strong>%1$s</strong>. %2$s ', 'theme-check' ), $filename, $error );
 			}
 		}
 
