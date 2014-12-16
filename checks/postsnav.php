357e734e6ea84ac4f853e541341b54b86efea62d
Otto <otto@ottodestruct.com>
2014-12-16 16:03:39 -0600

    Add support for the_posts_pagination
    Add support for the_posts_pagination, new to WP 4.1

--- checks/postsnav.php
+++ checks/postsnav.php
@@ -10,7 +10,9 @@ class PostPaginationCheck implements themecheck {
 		// combine all the php files into one string to make it easier to search
 		$php = implode( ' ', $php_files );
 		checkcount();
-		if ( strpos( $php, 'posts_nav_link' ) === false && strpos( $php, 'paginate_links' ) === false &&
+		if ( strpos( $php, 'posts_nav_link' ) === false && 
+		     strpos( $php, 'paginate_links' ) === false && 
+		     strpos( $php, 'the_posts_pagination' ) === false &&
 		   ( strpos( $php, 'previous_posts_link' ) === false && strpos( $php, 'next_posts_link' ) === false )
 		   ) {
 			$this->error[] = '<span class="tc-lead tc-required">'.__('REQUIRED','theme-check').'</span>: '.__("The theme doesn't have post pagination code in it. Use <strong>posts_nav_link()</strong> or <strong>paginate_links()</strong> or <strong>next_posts_link()</strong> and <strong>previous_posts_link()</strong> to add post pagination.", 'theme-check' );
