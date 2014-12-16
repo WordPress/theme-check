0063b3b4cf4b2c116b61f9de8ff9a5e95bf9d067
Otto <otto@ottodestruct.com>
2014-12-16 16:04:22 -0600

    Add support for title-tag check
    Add support for new add_theme_support('title-tag') for WP 4.1

--- checks/title.php
+++ checks/title.php
@@ -13,12 +13,19 @@ class Title_Checks implements themecheck {
 	function check( $php_files, $css_files, $other_files ) {
 		$ret = true;
 		$php = implode( ' ', $php_files );
-
+		
+		// Look for add_theme_support( 'title-tag' ) first
+		$titletag = true;
+		if ( ! preg_match( '#add_theme_support\s?\(\s?[\'|"]title-tag#', $php ) ) {
+			$this->error[] = '<span class="tc-lead tc-recommended">'.__('RECOMMENDED','theme-check').'</span>: '.__('No reference to <strong>add_theme_support( "title-tag", $args )</strong> was found in the theme. It is recommended that the theme implement this functionality for WordPress 4.1 and above.', 'theme-check' );
+			$titletag = false;
+		}
+		
 		/**
 		 * Look for <title> and </title> tags.
 		 */
 		checkcount();
-		if ( false === strpos( $php, '<title>' ) || false === strpos( $php, '</title>' ) ) {
+		if ( ( false === strpos( $php, '<title>' ) || false === strpos( $php, '</title>' ) ) && !$titletag  ) {
 			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The theme needs to have <strong>&lt;title&gt;</strong> tags, ideally in the <strong>header.php</strong> file.', 'theme-check' );
 			$ret = false;
 		}
@@ -27,7 +34,7 @@ class Title_Checks implements themecheck {
 		 * Check whether there is a call to wp_title().
 		 */
 		checkcount();
-		if ( false === strpos( $php, 'wp_title(' ) ) {
+		if ( false === strpos( $php, 'wp_title(' ) && !$titletag ) {
 			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check').'</span>: ' . __( 'The theme needs to have a call to <strong>wp_title()</strong>, ideally in the <strong>header.php</strong> file.', 'theme-check' );
 			$ret = false;
 		}
