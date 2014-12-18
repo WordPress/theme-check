57bbcfa955fcc39f40c128c7dca8b8493f40869c
Otto <otto@ottodestruct.com>
2014-12-18 11:20:12 -0600

    No args for title-tag

--- checks/title.php
+++ checks/title.php
@@ -17,7 +17,7 @@ class Title_Checks implements themecheck {
 		// Look for add_theme_support( 'title-tag' ) first
 		$titletag = true;
 		if ( ! preg_match( '#add_theme_support\s?\(\s?[\'|"]title-tag#', $php ) ) {
-			$this->error[] = '<span class="tc-lead tc-recommended">'.__('RECOMMENDED','theme-check').'</span>: '.__('No reference to <strong>add_theme_support( "title-tag", $args )</strong> was found in the theme. It is recommended that the theme implement this functionality for WordPress 4.1 and above.', 'theme-check' );
+			$this->error[] = '<span class="tc-lead tc-recommended">'.__('RECOMMENDED','theme-check').'</span>: '.__('No reference to <strong>add_theme_support( "title-tag" )</strong> was found in the theme. It is recommended that the theme implement this functionality for WordPress 4.1 and above.', 'theme-check' );
 			$titletag = false;
 		}
 		
