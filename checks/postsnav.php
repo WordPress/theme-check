380abba5ee4da04e3ef6b07d27b59e6ec0d74941
Otto <otto@ottodestruct.com>
2014-12-22 13:50:42 -0600

    Add support for the_posts_navigation as well

--- checks/postsnav.php
+++ checks/postsnav.php
@@ -13,9 +13,10 @@ class PostPaginationCheck implements themecheck {
 		if ( strpos( $php, 'posts_nav_link' ) === false && 
 		     strpos( $php, 'paginate_links' ) === false && 
 		     strpos( $php, 'the_posts_pagination' ) === false &&
+		     strpos( $php, 'the_posts_navigation' ) === false &&
 		   ( strpos( $php, 'previous_posts_link' ) === false && strpos( $php, 'next_posts_link' ) === false )
 		   ) {
-			$this->error[] = '<span class="tc-lead tc-required">'.__('REQUIRED','theme-check').'</span>: '.__("The theme doesn't have post pagination code in it. Use <strong>posts_nav_link()</strong> or <strong>paginate_links()</strong> or <strong>the_posts_pagination</strong> or <strong>next_posts_link()</strong> and <strong>previous_posts_link()</strong> to add post pagination.", 'theme-check' );
+			$this->error[] = '<span class="tc-lead tc-required">'.__('REQUIRED','theme-check').'</span>: '.__("The theme doesn't have post pagination code in it. Use <strong>posts_nav_link()</strong> or <strong>paginate_links()</strong> or <strong>the_posts_pagination()</strong> or or <strong>the_posts_navigation()</strong> or <strong>next_posts_link()</strong> and <strong>previous_posts_link()</strong> to add post pagination.", 'theme-check' );
 			$ret = false;
 		}
 
