4b1699a0e266cf0839cf7e299dcfc736f2900315
Otto <otto@ottodestruct.com>
2015-08-06 16:59:27 -0500

    Eliminate some of the old worm checks
    Most of these aren't around anymore, or at least uncommon. This whole
    file is fairly rubbish, and could use a rethink.

--- checks/worms.php
+++ checks/worms.php
@@ -9,13 +9,8 @@ class WormCheck implements themecheck {
 			'/wshell\.php/'=> __( 'This may be a script used by hackers to get control of your server!', 'theme-check' ),
 			'/ShellBOT/' => __( 'This may be a script used by hackers to get control of your server', 'theme-check' ),
 			'/uname -a/' => __( 'Tells a hacker what operating system your server is running', 'theme-check' ),
-			'/YW55cmVzdWx0cy5uZXQ=/' => __( 'base64 encoded text found in Search Engine Redirect hack <a href="http://blogbuildingu.com/wordpress/wordpress-search-engine-redirect-hack">[1]</a>', 'theme-check' ),
-			'/\$_COOKIE\[\'yahg\'\]/' => __( 'YAHG Googlerank.info exploit code <a href="http://creativebriefing.com/wordpress-hacked-googlerankinfo/">[1]</a>', 'theme-check' ),
-			'/ekibastos/' => __( 'Possible Ekibastos attack <a href="http://ocaoimh.ie/did-your-wordpress-site-get-hacked/">[1]</a>', 'theme-check' ),
-			'/<script>\/\*(GNU GPL|LGPL)\*\/ try\{window.onload.+catch\(e\) \{\}<\/script>/' => __( 'Possible "Gumblar" JavaScript attack <a href="http://threatinfo.trendmicro.com/vinfo/articles/securityarticles.asp?xmlfile=042710-GUMBLAR.xml">[1]</a> <a href="http://justcoded.com/article/gumblar-family-virus-removal-tool/">[2]</a>', 'theme-check' ),
 			'/php \$[a-zA-Z]*=\'as\';/' => __( 'Symptom of the "Pharma Hack" <a href="http://blog.sucuri.net/2010/07/understanding-and-cleaning-the-pharma-hack-on-wordpress.html">[1]</a>', 'theme-check' ),
 			'/defined?\(\'wp_class_support/' => __( 'Symptom of the "Pharma Hack" <a href="http://blog.sucuri.net/2010/07/understanding-and-cleaning-the-pharma-hack-on-wordpress.html">[1]</a>', 'theme-check' ),
-			'/AGiT3NiT3NiT3fUQKxJvI/' => __( 'Malicious footer code injection detected!', 'theme-check' )
 			);
 
 		foreach ( $php_files as $php_key => $phpfile ) {
