a07962a245e9b2cd2f790b7f36106db4f286edad
Carolina Nymark <myazalea@hotmail.com>
2016-01-02 09:19:05 +0100

    fix error
    missed a ,

--- checks/filenames.php
+++ checks/filenames.php
@@ -29,7 +29,7 @@ class File_Checks implements themecheck {
 				'error_log'				=> __( 'PHP error log', 'theme-check' ),
 				'web.config'			=> __( 'Server settings file', 'theme-check' ),
 				'\.sql'					=> __( 'SQL dump file', 'theme-check' ),
-				'__MACOSX'				=> __( 'OSX system file', 'theme-check' )
+				'__MACOSX'				=> __( 'OSX system file', 'theme-check' ),
 				'\.lubith'				=> __( 'Lubith theme generator file', 'theme-check' )
 				);
 
