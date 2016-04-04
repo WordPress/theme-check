531a04773dd24947797d01107d2bc42b9c2f60c7
pedro-mendonca <ped.gaspar@gmail.com>
2016-04-04 10:35:15 +0100

    Missing "!" was generating duplicate strings in lines 9 and 10.

--- checks/worms.php
+++ checks/worms.php
@@ -7,7 +7,7 @@ class WormCheck implements themecheck {
 		$php_files = array_merge( $php_files, $other_files );
 		$checks = array(
 			'/wshell\.php/'=> __( 'This may be a script used by hackers to get control of your server!', 'theme-check' ),
-			'/ShellBOT/' => __( 'This may be a script used by hackers to get control of your server', 'theme-check' ),
+			'/ShellBOT/' => __( 'This may be a script used by hackers to get control of your server!', 'theme-check' ),
 			'/uname -a/' => __( 'Tells a hacker what operating system your server is running', 'theme-check' ),
 			'/php \$[a-zA-Z]*=\'as\';/' => __( 'Symptom of the "Pharma Hack" <a href="http://blog.sucuri.net/2010/07/understanding-and-cleaning-the-pharma-hack-on-wordpress.html">[1]</a>', 'theme-check' ),
 			'/defined?\(\'wp_class_support/' => __( 'Symptom of the "Pharma Hack" <a href="http://blog.sucuri.net/2010/07/understanding-and-cleaning-the-pharma-hack-on-wordpress.html">[1]</a>', 'theme-check' ),
