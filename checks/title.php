d9d7ebbebd29f1102e99297ef8fcc3ed13816089
Ulrich Pogson <grapplerulrich@gmail.com>
2016-04-25 22:00:52 +0100

    Make add_theme_support('title-tag') required

--- checks/title.php
+++ checks/title.php
@@ -13,14 +13,15 @@ class Title_Checks implements themecheck {
 	function check( $php_files, $css_files, $other_files ) {
 		$ret = true;
 		$php = implode( ' ', $php_files );
-		
+
 		// Look for add_theme_support( 'title-tag' ) first
 		$titletag = true;
 		if ( ! preg_match( '#add_theme_support\s?\(\s?[\'|"]title-tag#', $php ) ) {
-			$this->error[] = '<span class="tc-lead tc-recommended">'.__('RECOMMENDED','theme-check').'</span>: '.__('No reference to <strong>add_theme_support( "title-tag" )</strong> was found in the theme. It is recommended that the theme implement this functionality for WordPress 4.1 and above.', 'theme-check' );
+			$this->error[] = '<span class="tc-lead tc-required">'.__('REQUIRED','theme-check').'</span>: '.__('No reference to <strong>add_theme_support( "title-tag" )</strong> was found in the theme.', 'theme-check' );
 			$titletag = false;
+			$ret = false;
 		}
-		
+
 		// Look for <title> and </title> tags.
 		checkcount();
 		if ( ( false === strpos( $php, '<title>' ) || false === strpos( $php, '</title>' ) ) && !$titletag  ) {
@@ -41,7 +42,7 @@ class Title_Checks implements themecheck {
 		foreach ( $php_files as $file_path => $file_content ) {
 			// Look for anything that looks like <svg>...</svg> and exclude it (inline svg's have titles too)
 			$file_content = preg_replace('/<svg>.*<\/svg>/s', '', $file_content);
-			
+
 			// First looks ahead to see of there's <title>...</title>
 			// Then performs a negative look ahead for <title> wp_title(...); </title>
 			if ( preg_match( '/(?=<title>(.*)<\/title>)(?!<title>\s*<\?php\s*wp_title\([^\)]*\);?\s*\?>\s*<\/title>)/s', $file_content ) ) {
