c5c10838f2aabefa991f82ffedf3ec3ed816c3cf
Keanan Koppenhaver <keanan@doejo.com>
2016-05-03 23:22:40 -0500

    Adding check for included .zip files

---
+++ checks/included-plugins.php
@@ -0,0 +1,40 @@
+<?php
+class IncludedPlugins implements themecheck {
+	protected $error = array();
+
+	function check( $php_files, $css_files, $other_files ) {
+		$ret = true;
+
+		$filenames = array();
+
+		foreach ( $php_files as $php_key => $phpfile ) {
+			array_push( $filenames, strtolower( basename( $php_key ) ) );
+		}
+		foreach ( $other_files as $php_key => $phpfile ) {
+			array_push( $filenames, strtolower( basename( $php_key ) ) );
+		}
+		foreach ( $css_files as $php_key => $phpfile ) {
+			array_push( $filenames, strtolower( basename( $php_key ) ) );
+		}
+
+		$blacklist = array(
+			'\.zip'	=> __( 'Zipped Plugin', 'theme-check' ),
+		);
+
+		checkcount();
+
+		foreach( $blacklist as $file => $reason ) {
+			if ( $filename = preg_grep( '/' . $file . '/', $filenames ) ) {
+				$error = implode( array_unique( $filename ), ' ' );
+				$this->error[] = sprintf( '<span class="tc-lead tc-required">'.__( 'REQUIRED','theme-check').'</span>: '.__( '<strong>Zip file found.</strong> Plugins are not allowed in themes. The zip file found was ', 'theme-check') . '<em>' . __( $error ) . '</em>.' ) ;
+				$ret = false;
+			}
+		}
+
+		return $ret;
+	}
+
+	function getError() { return $this->error; }
+}
+
+$themechecks[] = new IncludedPlugins;
