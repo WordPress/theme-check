7b04f4d2119c72e14462cb0e5fdc77a055f10638
ernilambar <nilambar@outlook.com>
2016-06-07 12:13:47 +0545

    Move HTML tag out of translation string

--- checks/navmenu.php
+++ checks/navmenu.php
@@ -18,7 +18,7 @@ class NavMenuCheck implements themecheck {
 		checkcount();
 		if ( preg_match( '/add_theme_support\s*\(\s?("|\')menus("|\')\s?\)/', $php ) ) {
 			/* translators: 1: function found, 2: function to be used */
-			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check') . '</span>: ' . sprintf( __( 'Reference to <strong>%1$s</strong> was found in the theme. This should be removed and %2$s used instead.', 'theme-check' ), 'add_theme_support( "menus" )','<a href="https://developer.wordpress.org/reference/functions/register_nav_menus/">register_nav_menus()</a>' );
+			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check') . '</span>: ' . sprintf( __( 'Reference to %1$s was found in the theme. This should be removed and %2$s used instead.', 'theme-check' ), '<strong>add_theme_support( "menus" )</strong>', '<a href="https://developer.wordpress.org/reference/functions/register_nav_menus/">register_nav_menus()</a>' );
 			$ret = false;
 		}
 
