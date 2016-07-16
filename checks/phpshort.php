019e69fd0402e65b41fd301932b7137966f4b0cd
xandros15 <xandros92@gmail.com>
2016-07-16 13:18:26 +0200

    #172 - add echo short tag support

--- checks/phpshort.php
+++ checks/phpshort.php
@@ -1,16 +1,20 @@
 <?php
 class PHPShortTagsCheck implements themecheck {
+
+	const ALLOW_SHORT_ECHO = true;
+
 	protected $error = array();
 
 	function check( $php_files, $css_files, $other_files ) {
 
 		$ret = true;
 
+		$regex = (self::ALLOW_SHORT_ECHO) ? '/<\?(?!php|xml|=)/i' : '/<\?(\=?)(?!php|xml)/i';
 		foreach ( $php_files as $php_key => $phpfile ) {
 			checkcount();
-			if ( preg_match( '/<\?(\=?)(?!php|xml)/i', $phpfile ) ) {
+			if ( preg_match( $regex, $phpfile ) ) {
 				$filename = tc_filename( $php_key );
-				$grep = tc_preg( '/<\?(\=?)(?!php|xml)/', $php_key );
+				$grep = tc_preg( $regex, $php_key );
 				$this->error[] = sprintf('<span class="tc-lead tc-warning">'.__('WARNING','theme-check').'</span>: '.__('Found PHP short tags in file %1$s.%2$s', 'theme-check'), '<strong>' . $filename . '</strong>', $grep);
 				$ret = false;
 			}
