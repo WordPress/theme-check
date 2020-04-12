2e49489d4a38c21dfce73a01d86504124c4512fa
Carolina Nymark <myazalea@hotmail.com>
2020-04-12 13:31:32 +0200

    Update adminbar.php
    Don't print minified files in the result.

--- checks/adminbar.php
+++ checks/adminbar.php
@@ -6,11 +6,12 @@ class NoHiddenAdminBar implements themecheck {
 	protected $error = array();
 
 	function check( $php_files, $css_files, $other_files ) {
-		$ret = true;
-		checkcount();
+		$ret      = true;
+
 		$php_regex = "/(add_filter(\s*)\((\s*)(\"|')show_admin_bar(\"|')(\s*)(.*))|(([^\S])show_admin_bar(\s*)\((.*))/";
 		$css_regex = "/(#wpadminbar)/";
 
+		checkcount();
 		// Check php files for filter show_admin_bar, show_admin_bar_front, and show_admin_bar().
 		foreach ( $php_files as $file_path => $file_content ) {
 
@@ -26,12 +27,18 @@ class NoHiddenAdminBar implements themecheck {
 			}
 		}
 
+		checkcount();
 		// Check CSS Files for #wpadminbar.
 		foreach ( $css_files as $file_path => $file_content ) {
 
 			$filename = tc_filename( $file_path );
 			$error    = '/#wpadminbar/';
-			$grep     = tc_preg( $error, $file_path );
+			// Don't print minified files.
+			if ( strpos( $filename, '.min.' ) === false ) {
+				$grep = tc_preg( $error, $file_path );
+			} else {
+				$grep = '';
+			}
 
 			if ( preg_match( $css_regex, $file_content, $matches ) ) {
 				$this->error[] = sprintf( '<span class="tc-lead tc-warning">' . __( 'WARNING', 'theme-check' ) . '</span>: ' . __( 'The theme is using `#wpadminbar` in %1$s. Hiding the admin bar is not allowed. This warning must be manually checked.', 'theme-check' ),
@@ -39,7 +46,7 @@ class NoHiddenAdminBar implements themecheck {
 			}
 		}
 
-	return $ret;
+		return $ret;
 	}
 
 	function getError() { return $this->error; }
