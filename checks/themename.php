eade0fa3ea1c33946f2a50ada825a3ac6b148fbd
Carolina Nymark <myazalea@hotmail.com>
2020-04-14 14:03:07 +0200

    Update value name, added check for name in style.css
    Update value name.
    Added a check for the name in style.css.
    Moved checkcount.
    Now returns true, to allow exceptions for old themes.

--- checks/themename.php
+++ checks/themename.php
@@ -8,7 +8,6 @@ class ThemeNameCheck implements themecheck {
 
 	function check( $php_files, $css_files, $other_files ) {
 
-		checkcount();
 		$ret = true;
 		global $themename;
 		global $data;
@@ -16,6 +15,10 @@ class ThemeNameCheck implements themecheck {
 		/* Remove the link added by get_theme_data_from_contents. */
 		$author = wp_strip_all_tags( $data['Author'] );
 
+		/* Get the name, as added in style.css */
+		$title = $data[ 'Title' ];
+
+		checkcount();
 		/* If the author is not the WordPress team, check for prohibited names. */
 		if ( 'the WordPress team' !== $author ) {
 
@@ -25,19 +28,25 @@ class ThemeNameCheck implements themecheck {
 			'twentytwentyseven', 'twentytwentyeight', 'twentytwentynine', 'twentythirty'
 			);
 
-			foreach ( $blacklist as $key ) {
-				if ( stripos( strtolower( preg_replace( '/[^a-z]/', '', $themename ) ), $key ) !== false ) {
-					$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check' ) . '</span>: ' . sprintf( __( 'Theme names in the Twenty* series are reserved for default themes. Found %1$s.', 'theme-check' ), '<strong>' . $key . '</strong>' );
-					$ret = false;
+			foreach ( $blacklist as $blacklisted_theme_name ) {
+				if ( stripos( strtolower( preg_replace( '/[^a-z]/', '', $themename ) ), $blacklisted_theme_name ) !== false ) {
+					$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check' ) . '</span>: ' . sprintf( __( 'Theme names in the Twenty* series are reserved for default themes. Found %1$s.', 'theme-check' ), '<strong>' . $blacklisted_theme_name . '</strong>' );
 				}
 			}
 		}
 
-		/* Check for prohibited words. */
+		checkcount();
+		/* Check for prohibited words in the folder name. */
 		if ( stripos( strtolower( preg_replace( '/[^a-z]/', '', $themename ) ), 'wordpress' ) !== false
 			|| stripos( strtolower( preg_replace( '/[^a-z]/', '', $themename ) ), 'theme' ) !== false ) {
 			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check' ) . '</span>: ' . __( 'Theme names should not contain the words theme or WordPress.', 'theme-check' );
-			$ret = false;
+		}
+
+		checkcount();
+		/* Check for prohibited words in style.css. */
+		if ( stripos( strtolower( preg_replace( '/[^a-z]/', '', $title ) ), 'wordpress' ) !== false
+			|| stripos( strtolower( preg_replace( '/[^a-z]/', '', $title ) ), 'theme' ) !== false ) {
+			$this->error[] = '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check' ) . '</span>: ' . __( 'Theme names should not contain the words theme or WordPress.', 'theme-check' );
 		}
 
 		return $ret;
