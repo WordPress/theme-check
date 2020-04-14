750351e243f06951b9bac4e60252d3f8fcae01fb
Carolina Nymark <myazalea@hotmail.com>
2020-04-14 06:08:54 +0200

    Update checks/filenames.php
    Co-Authored-By: Denis Žoljom <dingo-d@users.noreply.github.com>

--- checks/filenames.php
+++ checks/filenames.php
@@ -24,7 +24,7 @@ class File_Checks implements themecheck {
 			'thumbs\.db'          => __( 'Windows thumbnail store', 'theme-check' ),
 			'desktop\.ini'        => __( 'windows system file', 'theme-check' ),
 			'project\.properties' => __( 'NetBeans Project File', 'theme-check' ),
-			'project.xml'        => __( 'NetBeans Project File', 'theme-check' ),
+			'project\.xml'        => __( 'NetBeans Project File', 'theme-check' ),
 			'\.kpf'              => __( 'Komodo Project File', 'theme-check' ),
 			'^\.+[a-zA-Z0-9]'    => __( 'Hidden Files or Folders', 'theme-check' ),
 			'php.ini'            => __( 'PHP server settings file', 'theme-check' ),
