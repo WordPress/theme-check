4dfa41012b32d74b96c3f22ad244aa2dd00e852b
Carolina Nymark <myazalea@hotmail.com>
2020-04-18 05:55:07 +0200

    Updated check to return true

--- checks/deregister.php
+++ checks/deregister.php
@@ -17,8 +17,7 @@ class DeregisterCheck implements themecheck {
 					$grep = tc_preg( $error, $file_path );
 
 					$this->error[] = sprintf( '<span class="tc-lead tc-warning">' . __('WARNING','theme-check') . '</span>: ' . __( 'Found wp_deregister_script in %1$s. Themes must not deregister core scripts.', 'theme-check' ),
-						'<strong>' . $filename . '</strong>') . $grep;	
-					$ret = false;			
+						'<strong>' . $filename . '</strong>') . $grep;		
 				}
 			}
 			return $ret;
