c3a1d82bd73938bdf6e99df387b4d13045d3402b
Carolina Nymark <myazalea@hotmail.com>
2020-04-18 12:24:11 +0200

    Update favicon.php

--- checks/favicon.php
+++ checks/favicon.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Checks for favicons.
+ * Note that the check for the icon file is in filenames.php.
+ */
 
 class FaviconCheck implements themecheck {
 	protected $error = array();
@@ -6,16 +10,18 @@ class FaviconCheck implements themecheck {
 		function check( $php_files, $css_files, $other_files ) {
 
 		$ret = true;
-		
+
 		checkcount();
 
 		foreach ( $php_files as $file_path => $file_content ) {
 
 			$filename = tc_filename( $file_path );
 
-			if ( preg_match( '/(<link rel=[\'"]icon[\'"])|(<link rel=[\'"]apple-touch-icon-precomposed[\'"])|(<meta name=[\'"]msapplication-TileImage[\'"])/', $file_content, $matches ) ) {
-				$this->error[] = sprintf( '<span class="tc-lead tc-info">' . __('INFO','theme-check') . '</span>: ' . __( 'Possible Favicon found in %1$s. Favicons are handled by the Site Icon setting in the customizer since version 4.3.', 'theme-check' ), 
-					'<strong>' . $filename . '</strong>');				
+			if ( preg_match( '/(<link rel=[\'"]icon[\'"])|(<link rel=[\'"]shortcut icon[\'"])|(<link rel=[\'"]apple-touch-icon.*[\'"])|(<meta name=[\'"]msapplication-TileImage[\'"])/i', $file_content, $matches ) ) {
+				$this->error[] = sprintf( '<span class="tc-lead tc-required">' . __( 'REQUIRED', 'theme-check' ) . '</span>: ' . __( 'Possible Favicon found in %1$s. Favicons are handled by the Site Icon setting in the customizer since version 4.3.', 'theme-check' ),
+					'<strong>' . $filename . '</strong>'
+				);
+				$ret           = false;
 			}
 		}
 		return $ret;
@@ -23,4 +29,5 @@ class FaviconCheck implements themecheck {
 
 	function getError() { return $this->error; }
 }
-$themechecks[] = new FaviconCheck;
+
+$themechecks[] = new FaviconCheck();
