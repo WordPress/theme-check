26651d1f0d6f53764673624b87d8fe0a05a76662
Shakil-Shahadat <shakil.regi@gmail.com>
2020-09-12 16:05:04 +0600

    Adds a missing space

--- checks/postthumb.php
+++ checks/postthumb.php
@@ -16,7 +16,7 @@ class PostThumbnailCheck implements themecheck {
 		}
 
 		if ( strpos( $php, 'post-thumbnails' ) === false ) {
-			$this->error[] = '<span class="tc-lead tc-recommended">'.__('RECOMMENDED','theme-check').'</span>: '.__('No reference to post-thumbnails was found in the theme. If the theme has a thumbnail like functionality, it should be implemented with <strong>add_theme_support( "post-thumbnails" )</strong>in the functions.php file.', 'theme-check' );
+			$this->error[] = '<span class="tc-lead tc-recommended">'.__('RECOMMENDED','theme-check').'</span>: '.__('No reference to post-thumbnails was found in the theme. If the theme has a thumbnail like functionality, it should be implemented with <strong>add_theme_support( "post-thumbnails" )</strong> in the functions.php file.', 'theme-check' );
 		}
 
 		return $ret;
