53413a1d1e7bd2c24db867417eae4d275002d151
Fränk Klein <frank@humanmade.co.uk>
2020-11-05 10:47:34 +0100

    Remove required check for avatar display support.

--- checkbase.php
+++ checkbase.php
@@ -344,6 +344,7 @@ function tc_adapt_checks_for_fse_themes( $php_files, $css_files, $other_files ) 
 			|| $check instanceof TagCheck
 			|| $check instanceof Style_Needed
 			|| $check instanceof WidgetsCheck
+			|| $check instanceof GravatarCheck
 		) {
 			unset( $themechecks[ $key ] );
 		}
