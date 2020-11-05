27abbe55f271084a30f039df55e3ad060aded972
Fränk Klein <frank@humanmade.co.uk>
2020-11-05 10:56:40 +0100

    Remove required comment reply scriopt check.

--- checkbase.php
+++ checkbase.php
@@ -348,6 +348,7 @@ function tc_adapt_checks_for_fse_themes( $php_files, $css_files, $other_files ) 
 			|| $check instanceof Title_Checks
 			|| $check instanceof PostPaginationCheck
 			|| $check instanceof CommentPaginationCheck
+			|| $check instanceof Comment_Reply
 		) {
 			unset( $themechecks[ $key ] );
 		}
