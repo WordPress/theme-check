1144b1af07a9ce65b887e4333e702efde6f42150
Fränk Klein <frank@humanmade.co.uk>
2020-11-05 10:52:21 +0100

    Remove required hardcoded page title checks.

--- checkbase.php
+++ checkbase.php
@@ -345,6 +345,7 @@ function tc_adapt_checks_for_fse_themes( $php_files, $css_files, $other_files ) 
 			|| $check instanceof Style_Needed
 			|| $check instanceof WidgetsCheck
 			|| $check instanceof GravatarCheck
+			|| $check instanceof Title_Checks
 		) {
 			unset( $themechecks[ $key ] );
 		}
