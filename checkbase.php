bddf05a20380cac1c779d6f109cf96334c53924a
Fränk Klein <frank@humanmade.co.uk>
2020-11-05 12:35:15 +0100

    Remove recommended check for custom header and custom background.

--- checkbase.php
+++ checkbase.php
@@ -352,6 +352,7 @@ function tc_adapt_checks_for_fse_themes( $php_files, $css_files, $other_files ) 
 			|| $check instanceof Basic_Checks
 			|| $check instanceof NavMenuCheck
 			|| $check instanceof PostThumbnailCheck
+			|| $check instanceof CustomCheck
 		) {
 			unset( $themechecks[ $key ] );
 		}
