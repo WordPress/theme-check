731fb369442149f86545a46b2a713a7d7c66259e
Dion Hulse <dion@wordpress.org>
2020-12-07 12:55:09 +1000

    Standardise Google advertising code detection

--- checks/badthings.php
+++ checks/badthings.php
@@ -47,7 +47,7 @@ class Bad_Checks implements themecheck {
 
 		$checks = array(
 			'/cx=[0-9]{21}:[a-z0-9]{10}/' => __( 'Google search code detected', 'theme-check' ),
-			'/pub-[0-9]{16}/'             => __( 'Google advertising code detected', 'theme-check' ),
+			'/pub-[0-9]{16}/i'            => __( 'Google advertising code detected', 'theme-check' ),
 		);
 
 		foreach ( $other_files as $php_key => $phpfile ) {
