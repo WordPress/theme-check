3cb103750b653969ba1eeffbad966e0d43b2617a
Carolina Nymark <myazalea@hotmail.com>
2021-05-21 13:37:31 +0200

    Split Style_Needed into two checks

--- checks/style-css-header.php
+++ checks/style-css-header.php
@@ -1,5 +1,5 @@
 <?php
-class Style_Needed implements themecheck {
+class Style_CSS_Header implements themecheck {
 	protected $error = array();
 
 	function check( $php_files, $css_files, $other_files ) {
@@ -17,7 +17,6 @@ class Style_Needed implements themecheck {
 			'[ \t\/*#]*Text Domain:'  => __( '<strong>Text Domain:</strong> is missing from your style.css header.', 'theme-check' ),
 			'[ \t\/*#]*Tested up to:' => __( '<strong>Tested up to:</strong> is missing from your style.css header. Also, this should be numbers only, so <em>5.0</em> and not <em>WP 5.0</em>', 'theme-check' ),
 			'[ \t\/*#]*Requires PHP:' => __( '<strong>Requires PHP:</strong> is missing from your style.css header.', 'theme-check' ),
-			'\.screen-reader-text'    => __( '<strong>.screen-reader-text</strong> css class is needed in your theme css. See See: <a href="http://codex.wordpress.org/CSS#WordPress_Generated_Classes">the Codex</a> for an example implementation.', 'theme-check' ),
 		);
 
 		foreach ( $checks as $key => $check ) {
@@ -38,4 +37,4 @@ class Style_Needed implements themecheck {
 		return $this->error;
 	}
 }
-$themechecks[] = new Style_Needed();
+$themechecks[] = new Style_CSS_Header();
