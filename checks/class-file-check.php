f268bd5fc1674263af66fc13f5328376a4b32032
KafleG <kafleg@git.wordpress.org>
2022-06-03 15:02:37 +0545

    Update class-file-check
    Checked the required requirement of index.php from block themes

--- checks/class-file-check.php
+++ checks/class-file-check.php
@@ -66,7 +66,20 @@ class File_Check implements themecheck {
 			'favicon\.ico'        => __( 'Favicon', 'theme-check' ),
 		);
 
-		$musthave = array( 'style.css', 'readme.txt' );
+		$musthave = array( 'index.php', 'style.css', 'readme.txt' );
+
+		$fse_find = array_filter( array_keys( $other_files ), function( $file_name ) {
+			if ( false !== stripos( $file_name, 'templates/index.html' ) || false !== stripos( $file_name, 'block-templates/index.html' ) ) {
+				return true;
+			}
+
+			return false;
+		} );
+
+		if ( ! empty( $fse_find ) ) {
+			$fse_not_needed = array_search( 'index.php', $musthave );
+			unset( $musthave[ $fse_not_needed ] );
+		}
 
 		checkcount();
 
